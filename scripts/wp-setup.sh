1960639dfe3bcaacb3f1fcf334af476925371806
Juhi <dpjkkd@gmail.com>
2025-10-05 08:45:36 -0400

    spv

--- scripts/wp-setup.sh
+++
@@ -1,6 +0,0 @@
-#/bin/bash
-# pip install requests markdown pyyaml python-slugify
-export WP_SITE="https://spherevista360.com"
-export WP_USER="JK"
-export WP_APP_PASS="R8sj tOZG 8ORr ntSZ XlPt qTE9"
-python wp_agent_bulk.py /absolute/path/to/posts_to_upload
